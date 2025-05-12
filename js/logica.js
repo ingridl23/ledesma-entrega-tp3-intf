891839cc2def0ef2b7a9bb2509edb1d4fe99e481
Ingrid <tu-correo@ejemplo.com>
2025-05-12 12:07:54 -0300

    se agrega animacion al botron enviar

--- js/logica.js
+++ js/logica.js
@@ -1,9 +1,41 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     const btnMenu = document.querySelector(".btn_menu");
     const menuList = document.querySelector(".lista-container-nav");
 
     btnMenu.addEventListener("click", () => {
-      menuList.classList.toggle("show");
+        menuList.classList.toggle("show");
     });
-  });
+
+    const formulario = document.querySelector(".form");
+    const submitBtn = formulario.querySelector(".submit");
+    const errorMsg = formulario.querySelector(".error-msg");
+
+    formulario.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const telefono = document.getElementById("telefono").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const mensaje = document.getElementById("mensaje").value.trim();
+
+        submitBtn.classList.remove("click", "error");
+        errorMsg.classList.remove("show");
+
+        if (telefono === "" || email === "" || mensaje === "") {
+            // Mostrar cruz roja y mensaje de error
+            submitBtn.classList.add("error");
+            errorMsg.classList.add("show");
+        } else {
+            // Mostrar tilde verde
+            submitBtn.classList.add("click");
+            submitBtn.disabled = true;
+
+            setTimeout(() => {
+                submitBtn.classList.remove("click");
+                submitBtn.disabled = false;
+                formulario.reset();
+            }, 2000);
+
+
+        }
+    });
+});
